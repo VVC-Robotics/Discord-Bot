1bbea1d7494cf3a9512495c6c8781c6a2c66eb1c
MrDoritos <ianmoore322@gmail.com>
2025-03-19 00:51:17 -0700

    Simplify

--- main.cpp
+++ main.cpp
@@ -111,20 +111,44 @@ struct Program {
 
     void add_role(dpp::snowflake guild, dpp::snowflake user, dpp::snowflake role) {
         log("Adding role %lu to user %lu in guild %lu\n", role, user, guild);
+
         bot.guild_member_add_role(guild, user, role, complete_handler);
     }
 
+    void create_role(dpp::snowflake guild, dpp::snowflake user, std::string role_name) {
+        log("Creating role \"%s\" for user %lu in guild %lu\n", role_name.c_str(), user, guild);
+
+        bot.role_create(dpp::role().set_name(role_name).set_guild_id(guild), [&,guild,user,role_name](dpp::confirmation_callback_t e) {
+            if (e.is_error()) { handle_apierror(e.get_error()); return; }
+
+            auto role = std::get<dpp::role>(e.value);
+
+            add_role(guild, user, role.id);
+        });
+    }
+
+    void add_or_create_role(dpp::snowflake guild, dpp::snowflake user, std::string role_name) {
+        bot.roles_get(guild, [&,guild,user,role_name](dpp::confirmation_callback_t e) {
+            if (e.is_error()) { handle_apierror(e.get_error()); return; }
+
+            auto &roles = std::get<dpp::role_map>(e.value);
+            auto iter = std::find_if(roles.begin(), roles.end(), [role_name](const auto &p) { return p.second.name == role_name; });
+
+            if (iter != roles.end())
+                add_role(guild, user, (*iter).second.id);
+            else
+                create_role(guild, user, role_name);
+        });
+    }
+
     dpp::message create_welcome_message(std::string user_mention, dpp::snowflake channel_id) {
-        //std::string str = "Welcome " + user_mention + "!\n\nReact to the checkbox to become verified";
-        //auto m = dpp::message(channel_id, str);
-        //auto m = dpp::message().set_flags(dpp::m_using_components_v2);
         auto m = dpp::message();
-        //m.set_flags(dpp::message_flags::m_using_components_v2);
+
         m.set_channel_id(channel_id);
         m.set_content("Welcome " + user_mention + "!\n\nClick the button to become verified!");
+        
+        // Set later
         //m.set_flags(dpp::m_ephemeral);
-        //auto emoji = dpp::component().set_type(dpp::component_type::cot_button).set_emoji("☑️");
-        //m.add_component(emoji);
 
         m.add_component(dpp::component()
             .add_component(dpp::component()
@@ -134,73 +158,6 @@ struct Program {
             .set_id("verify_button"))
         );
 
-        /*        
-        m.add_component(
-            dpp::component()
-            //.set_type(dpp::cot_)
-            //.set_accent(dpp::utility::rgb(37, 0, 221))
-            .add_component(
-            dpp::component()
-                //.set_type(dpp::cot_section)
-                .add_component(
-                    dpp::component()
-                        .set_id("text")
-                        .set_type(dpp::cot_text)
-                        .set_content("Click to automatically verify your account on the VVC Robotics server!")
-                )
-                .set_accessory(
-                    dpp::component()
-                        .set_type(dpp::cot_button)
-                        .set_label("Verify")
-                        .set_style(dpp::cos_primary)
-                        .set_id("button")
-                )
-            )
-        );
-        */
-        
-
-        /*
-        m
-	            .set_flags(dpp::m_using_components_v2).add_component_v2(
-	            dpp::component()
-	                .set_type(dpp::cot_container)
-	                .set_accent(dpp::utility::rgb(255, 0, 0))
-	                .set_spoiler(true)
-	                .add_component_v2(
-	                    dpp::component()
-	                        .set_type(dpp::cot_section)
-	                        .add_component_v2(
-	                            dpp::component()
-	                                .set_type(dpp::cot_text_display)
-	                                .set_content("Click if you love cats")
-	                        )
-	                        .set_accessory(
-	                            dpp::component()
-	                                .set_type(dpp::cot_button)
-	                                .set_label("Click me")
-	                                .set_style(dpp::cos_danger)
-	                                .set_id("button")
-	                        )
-	                )
-	        ).add_component_v2(
-	            dpp::component()
-	                .set_type(dpp::cot_separator)
-	                .set_spacing(dpp::sep_large)
-	                .set_divider(true)
-	        ).add_component_v2(
-	            dpp::component()
-	                .set_type(dpp::cot_media_gallery)
-	                .add_media_gallery_item(
-	                    dpp::component()
-	                        .set_type(dpp::cot_thumbnail)
-	                        .set_description("A cat")
-	                        .set_thumbnail("https://www.catster.com/wp-content/uploads/2023/11/Beluga-Cat-e1714190563227.webp")
-	                )
-	        );
-            */
-
-        //message_create(m);
         return m;
     }
 };
@@ -240,24 +197,6 @@ int main() {
             //message_create.reply(prog.create_welcome_message(user.get_mention(), msg.channel_id), true, prog.complete_handler);
     });
 
-    bot.on_message_reaction_add([&](const message_reaction_add_t &message_reaction_add) {
-        auto &channel = message_reaction_add.channel_id;
-        auto &user = message_reaction_add.reacting_user;
-
-        if (channel != verification_channel)
-            return;
-
-        if (message_reaction_add.message_author_id != bot_id)
-            return;
-
-        if (!message_reaction_add.reacting_guild.id)
-            return;
-
-        auto &guild = message_reaction_add.reacting_guild;
-
-        bot.guild_member_add_role(guild.id, user.id, role_id);
-    });
-
     bot.on_button_click([&](const button_click_t &button_click) {
         auto &id = button_click.custom_id;
         auto &command = button_click.command;
@@ -267,35 +206,13 @@ int main() {
 
         auto &user = button_click.command.member;
         auto user_id = user.user_id;
-        //auto guild_id = command.guild_id;
         auto guild_id = user.guild_id;
 
         prog.log("Button clicked: %s by %lu\n", id.c_str(), user_id);
 
-        if (id != "verify_button")
-            return;
+        if (id == "verify_button")
+            prog.add_or_create_role(guild_id, user_id, "Verified");
 
-        prog.log("get roles in guild %lu\n", guild_id);
-
-        bot.roles_get(guild_id, [&prog,guild_id,user_id](confirmation_callback_t e) {
-            if (e.is_error()) { prog.handle_apierror(e.get_error()); return; }
-
-            auto &roles = std::get<role_map>(e.value);
-
-            auto iter = std::find_if(roles.begin(), roles.end(), [](const auto &p){return p.second.name == "Verified";});
-            if (iter != roles.end()) {
-                prog.log("assign role in guild %lu\n", guild_id);
-                prog.add_role(guild_id, user_id, (*iter).second.id);
-            } else {
-                prog.log("create role in guild %lu\n", guild_id);
-                prog.bot.role_create(role().set_name("Verified").set_guild_id(guild_id), [&prog,guild_id,user_id](confirmation_callback_t _e) {
-                    if (_e.is_error()) { prog.handle_apierror(_e.get_error()); return; }
-
-                    role _role = std::get<role>(_e.value);
-                    prog.add_role(guild_id, user_id, _role.id);
-                });
-            }
-        });
     });
 
     bot.start(dpp::st_wait);
